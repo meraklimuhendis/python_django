cb666a3e4b0055eee1bae9ef8825d235de040e0d
Meraklı Mühendis <myagizorhan@hotmail.com>
2024-01-21 16:48:52 +0300

    Güncellendi - docker_container

--- docker_container/django_project/urls.py
+++ docker_container/django_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_docker project.
+URL configuration for django_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -11,7 +11,7 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django_project.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
